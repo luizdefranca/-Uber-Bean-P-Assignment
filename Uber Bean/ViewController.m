f12edadd84278b5d4bbe9fe40cf8916bb92053cd
LuizRamos <luizramospe@hotmail.com>
2019-05-17 15:27:48 -0400

    End Assignment

--- Uber Bean/ViewController.m
+++ Uber Bean/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+@import MapKit;
+@import CoreLocation;
 
-@interface ViewController ()
+
+@interface ViewController ()<CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (nonatomic, strong) CLLocationManager *locationManager;
 
 @end
 
@@ -16,8 +21,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self requestLocationPermission];
+    self.locationManager.delegate = self;
+
 }
 
+-(void) requestLocationPermission {
+    self.locationManager = [CLLocationManager new];
+    [self.locationManager requestWhenInUseAuthorization];
+    self.mapView.showsUserLocation = YES;
+}
 
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+
+    CLLocation *location = locations[0];
+
+    MKCoordinateRegion region =MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(0.06, 0.06));
+
+
+
+    [self.mapView setRegion: region animated:YES];
+
+
+     NSLog(@"%@", locations[0]);
+}
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"%@", error);
+}
+
+-(void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+
+    if(status == kCLAuthorizationStatusAuthorizedWhenInUse ||
+       status == kCLAuthorizationStatusAuthorizedAlways){
+         [self.locationManager requestLocation];
+    }
+}
 @end
+
+
